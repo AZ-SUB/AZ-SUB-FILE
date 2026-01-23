501954c1ee023c2db9e90d53024d95bb70392834
archie <archieverania1130@gmail.com>
2026-01-23 11:57:00 +0800

    Create textUtils.js

---
+++ src/utils/textUtils.js
@@ -0,0 +1,14 @@
+/**
+ * Converts a string to title case
+ * @param {string} str - The string to convert
+ * @returns {string} The string in title case
+ */
+export const toTitleCase = (str) => {
+    if (!str) return '';
+    
+    return str
+        .toLowerCase()
+        .split(' ')
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' ');
+};
